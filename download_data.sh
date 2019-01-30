739e08a26ad80fbba886fe4b58ada930a848ccf9
semueller <semueller@techfak.uni-bielefeld.de>
2019-01-30 09:36:52 +0100

    shell script for downloading relevant data

---
+++ download_data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir data
+cd data
+
+mkdir tf
+cd tf
+wget http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz
+tar -xvzf speech_commands_v0.02.tar.gz
+rm speech_commands_v0.02.tar.gz
+
+cd ../../
