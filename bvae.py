545a8a5deb3da33d9fb7aafa2ac6cb33064c5307
semueller <semueller@techfak.uni-bielefeld.de>
2019-02-18 17:27:15 +0100

    bvae test check if gpu is available

--- bvae.py
+++ bvae.py
@@ -229,6 +229,14 @@ if __name__ == '__main__':
     else:
         root_dir = './data/test/'
 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(0) / 1024 ** 3, 1), 'GB')
+
     train = MNIST(root=root_dir, download=True, train=True)
     test = MNIST(root=root_dir, download=True, train=False)
     # num_train_samples = len(train)
