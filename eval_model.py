0151e90c6cda6652c17321ac25c867e8615069ed
Sebastian Mueller <semueller@techfak.uni-bielefeld.de>
2019-03-23 19:04:05 +0100

    eval_model saves plots and distances/ umap embedding now to pngs/ pickled objects

--- eval_model.py
+++ eval_model.py
@@ -7,38 +7,40 @@ from sklearn.metrics.pairwise import euclidean_distances
 from umap import UMAP
 
 import matplotlib.pyplot as plt
+import pickle as pkl
 
 if __name__=='__main__':
     modelpath = '/home/bing/sdb/models'
     modelname = 'RbVAE_mfccs_100_500_100'
     datapath = '/home/bing/sdb/testsets/mfccs_50spc.pkl'
+    dataset_name = datapath.split('/')[-1].split('.')[0]
+
+
+    show = True
+    save = True
 
     data = pkl.load(open(datapath, 'rb'))
 
     data['X'] = torch.tensor(data['X'], dtype=torch.float)  # jibbles..
     model = load_model(modelpath, modelname)
 
-    def embed(model, x):
-        mu_, log_var_, h_n = model.encode(x)
-        return mu_ + torch.exp(log_var_ / 2)  # a sample without epsilon
-
-
     print('compute encoding')
-    embedding = embed(model, data['X'])
+    embedding = model.embed(data['X'])
     embedding = embedding.detach().numpy()
-    def calc_dist_matric(X):
-        return euclidean_distances(X)
+
     print('computing {} distances'.format(len(embedding) ** 2))
-    distances = calc_dist_matric(embedding)
-    print(distances)
+    distances = euclidean_distances(embedding)
     f1 = plt.figure()
     plt.imshow(distances)
 
-    print('calc dr with umap')
+    if save:
+        plt.savefig('distances_{}.png'.format(dataset_name))
+        pkl.dump(distances, open('distances_{}.pkl'.format(dataset_name), 'wb'))
+
+    print('calc dimensionality reduction with umap')
     um = UMAP()
     dr_embedding = um.fit_transform(embedding)
 
-    print('plot')
     classes = np.unique(data['Y'])
     num_classes = len(classes)
     codebook = {c: n for c, n in zip(classes, range(num_classes))}
@@ -48,6 +50,10 @@ if __name__=='__main__':
     colors = [colors[i] for i in coded]
     f2 = plt.figure()
     plt.scatter(
-        embedding[:, 0], embedding[:, 1], c=colors, cmap="Spectral", s=10
+        dr_embedding[:, 0], dr_embedding[:, 1], c=colors, cmap="Spectral", s=10
     )
-    plt.show()
+    if save:
+        plt.savefig('umap_scatter_{}'.format(dataset_name))
+        pkl.dump(dr_embedding, 'umap_embedding_{}.pkl'.format)
+    if show:
+        plt.show()
