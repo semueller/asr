77f1d336b298b609ef48cc689ec2ae499306003d
semueller <semueller@techfak.uni-bielefeld.de>
2019-03-29 13:35:30 +0100

    checking for gpu in dataset_to_hidden_states

--- dataset_to_hidden_states.py
+++ dataset_to_hidden_states.py
@@ -1,36 +1,37 @@
-import pickle as pkl
-from asr.util import load_model, to_categorical, test_classifier, get_filenames
+from asr.util import load_model, to_categorical, test_classifier, get_filenames, check_for_gpu
 import click
 
 import torch
-import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
-from umap import UMAP
+#import numpy as np
+#from sklearn.metrics.pairwise import euclidean_distances
+#from umap import UMAP
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pickle as pkl
-import os
+#import os
 
 
 @click.command()
-@click.option('--data', type=str, default='./path/to/data.pkl', help='expects path to pickle containing a dict with the word as key')
-@click.option('--models', type=str, default='./path/to/folder/with/models', help='expects path to pickle containing a dict with the word as key')
+@click.option('--datapath', type=str, default='./path/to/data.pkl', help='expects path to pickle containing a dict with the word as key')
+@click.option('--modelpath', type=str, default='./path/to/folder/with/models', help='expects path to pickle containing a dict with the word as key')
 def main(modelpath, datapath, dataset_name='mfccs.pkl'):
     modelnames = get_filenames(modelpath, substr='tensor')
     # dataset_name = 'mfccs.pkl'
 
     data = pkl.load(open(datapath+dataset_name, 'rb'))
-    data['X'] = torch.tensor(data['X'], dtype=torch.float)  # jibbles..
+    x = torch.tensor(data['X'], dtype=torch.float)  # jibbles..
+    device = check_for_gpu()
+    if device.type == 'cuda':
+        x = x.to(device)
     # Y_cat = torch.tensor(to_categorical(data['Y']), dtype=torch.float32)
 
     for modelname in modelnames:
 
-        print(f'\n\n\nevaluating {modelname}')
-        model = load_model(modelpath, modelname)
+        model = load_model(modelpath, modelname, inference_only=True)
 
         print('compute encoding')
-        _, h_t = model.forward(data['X'])
-
+        _, h_t = model.forward(x)
+        print('saving')
         result = {}
         result['X'] = h_t
         result['Y'] = data['Y']
