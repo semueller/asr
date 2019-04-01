1c52b3ec747dda8127ca202e25239d1f17679538
semueller <semueller@techfak.uni-bielefeld.de>
2019-04-01 15:49:49 +0200

    small script to train gmlvq model on embedded data and save it

---
+++ train_gmlvq_embedding.py
@@ -0,0 +1,31 @@
+import click
+
+from sklearn_lvq import GmlvqModel
+
+import pickle as pkl
+import numpy as np
+
+from asr.util import to_categorical
+
+
+@click.command()
+@click.option('--datapath', type=str, default='./path/to/data.pkl', help='expects path to pickle containing a dict with the word as key')
+@click.option('--modelpath', type=str, default='./path/to/folder/with/models', help='expects path to pickle containing a dict with the word as key')
+def main(modelpath, datapath, dataset_name='mfccs.pkl'):
+    data = pkl.load(open(datapath, 'rb'))
+
+    X = data['X'].detach().numpy()
+    Y = np.argmax(to_categorical(data['Y']), 1)
+
+    gmlvq = GmlvqModel()
+    print('train gmlvq')
+    gmlvq.fit(X, Y)
+    print('done, compute egenvalue decomposition of gmlvq.omega**2')
+    pkl.dump(gmlvq, open(modelpath+'/gmlvq_500.pkl', 'wb'))
+    v, u = np.linalg.eig(gmlvq.omega_.conj().T.dot(gmlvq.omega_))
+    print(v, u)
+    pass
+
+if __name__=='__main__':
+    main()
+    exit(0)
