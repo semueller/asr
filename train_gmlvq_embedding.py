451a340903544622917ad90fa303c65843664beb
semueller <semueller@techfak.uni-bielefeld.de>
2019-04-18 14:52:06 +0200

    trianing script for gmlvq training multiple models now

--- train_gmlvq_embedding.py
+++ train_gmlvq_embedding.py
@@ -5,27 +5,47 @@ from sklearn_lvq import GmlvqModel
 import pickle as pkl
 import numpy as np
 
-from asr.util import Dataset
+from asr.util import gmlvq_covered_variance, Dataset, load_pkl, get_filenames, Dataset
 
+def get_error(lvq_model: GmlvqModel, x, y) -> float:
+    y_ = lvq_model.predict(x)
+    errors = 1 - np.mean(y_ == y)
+    return errors
 
 @click.command()
-@click.option('--datapath', type=str, default='./path/to/data.pkl', help='expects path to pickle containing a dict with the word as key')
-@click.option('--modelpath', type=str, default='./path/to/folder/with/models', help='expects path to pickle containing a dict with the word as key')
-def main(modelpath, datapath, dataset_name='mfccs.pkl'):
-    dataset: Dataset = pkl.load(open(datapath, 'rb'))
+@click.option('--datapath', type=str, default='./path/to/data.pkl', help='expects path to Dataset containing embeddings')
+@click.option('--modelpath', type=str, default='./path/to/ave/models/in', help='where to save trained models')
+@click.option('--idxs', type=str, default='./train_test_idxs.pkl',
+              help='dict with train test keys containing list of indices')
+def main(datapath, modelpath, idxs):
+    dataset_names = get_filenames(datapath)
+    print(f'using sets {dataset_names} from {datapath}')
+    print('looking for idx files')
+    idxs = load_pkl(idxs)
+    idxs_train = idxs['train']
+    idxs_test = idxs['test']
 
-    X = dataset.data.detach().numpy()
-    Y = dataset.get_labels_numerical()
-    hiddim = datapath.split('/')[-1].split('_')[2]
-    print(hiddim)
-    gmlvq = GmlvqModel()
-    print('train gmlvq')
-    gmlvq.fit(X, Y)
-    print('done, compute egenvalue decomposition of gmlvq.omega**2')
-    pkl.dump(gmlvq, open(modelpath+f'/gmlvq{hiddim}.pkl', 'wb'))
-    v, u = np.linalg.eig(gmlvq.omega_.conj().T.dot(gmlvq.omega_))
-    print(v, u)
-    pass
+    for dataset_name in dataset_names:
+        dataset: Dataset = load_pkl(dataset_name)
+        X = dataset.data.detach().numpy()
+        Y = dataset.labels.get_labels_numerical()
+        x_train, x_test = X[idxs_train], X[idxs_test]
+        y_train, y_test = Y[idxs_train], Y[idxs_test]
+        hiddim = dataset_name.split('/')[-1].split('_')[2]
+        print(f'training gmlvq on {hiddim} dim embedding')
+        gmlvq = GmlvqModel()
+        gmlvq.fit(x_train, y_train)
+        train_error = get_error(gmlvq, x_train, y_train)
+        test_error = get_error(gmlvq, x_test, y_test)
+        var = gmlvq_covered_variance(gmlvq, thresh=1, verbose=True)
+        misc = {'train_error': train_error, 'test_error' : test_error, 'matrix_var' : var}
+        print(f'adding misc data to gmlvq model {misc}')
+        gmlvq.misc = misc
+        modelname = f'gmlvq{hiddim}.pkl'
+        print(f'saving model to {modelname}')
+        pkl.dump(gmlvq, open(modelpath+modelname, 'wb'))
+
+
 
 if __name__=='__main__':
     main()
